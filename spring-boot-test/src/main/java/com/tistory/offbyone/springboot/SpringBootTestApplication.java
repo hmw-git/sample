9ac4731cc6cf5896ae93dc6e6da7ffb7132443c8
pron <pron@DESKTOP-E4D2NQV>
2020-07-30 12:56:21 +0900

    edit

--- spring-boot-test/src/main/java/com/tistory/offbyone/springboot/SpringBootTestApplication.java
+++ spring-boot-test/src/main/java/com/tistory/offbyone/springboot/SpringBootTestApplication.java
@@ -2,12 +2,20 @@ package com.tistory.offbyone.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootTestApplication {
+public class SpringBootTestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTestApplication.class, args);
 	}
+	
+	@Override 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { 
+		return builder.sources(SpringBootTestApplication.class); 
+	}
+
 
 }
